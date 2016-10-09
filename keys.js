ee276eddabbac9f5d19865c50ad6e82948f362c9
Carlos Lopez <clo2go@gmail.com>
2016-10-08 20:50:04 -0500

    stuff

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_key: 'J7qoqi24E5rnj31PWqB3aEpW2',
+	consumer_secret: 'Z6Nb3PzjdkigacofpCHcRVIbe7uSqJLmHEtmt4RClYXVZBc0Me',
+	access_token_key: '17090374-LG5gBbwOPbskl2TQLYpxl7Fv4RnNbNLJW3lew3jwg',
+	access_token_secret: 'uaj7Hrzu0OzB4ItSUgz48JpgrHfd274JF9PUkL3Hfb6Sb',
+
+}
